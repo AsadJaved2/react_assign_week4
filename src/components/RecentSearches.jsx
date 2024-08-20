87a80944c609680d4d3d70e7b79eceb374efcdd5
AsadJaved2 <asadjaved744@gmail.com>
2024-08-20 16:52:32 +0500

    refactor 1

--- src/components/RecentSearches.jsx
+++ src/components/RecentSearches.jsx
@@ -8,6 +8,7 @@ export default function RecentSearches() {
 
   return (
     <div className="max-w-md mx-auto mt-4 bg-white shadow-md rounded-lg p-4">
+      <h1 className="font-bold">Recents</h1>
       {result.payload && result.payload.weather && Array.isArray(result.payload.weather.recentSearches) ? (
         <ul className="list-disc space-y-2 pl-5">
           {result.payload.weather.recentSearches.map((item, index) => (
